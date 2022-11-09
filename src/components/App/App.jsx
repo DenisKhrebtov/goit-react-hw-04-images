87b95481f609c0e5bb1d53c7ca49ec3569980299
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-11-09 21:13:35 +0200

    add 4.3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import { toast } from 'react-toastify';
 
@@ -12,67 +12,124 @@ import { Toast } from '../ToastContainer/ToastContainer';
 
 import { Wrapper } from './App.styled';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    total: 0,
-    loading: false,
-  };
+export function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = false;
 
-  handleFormSubmit = queryNew => {
-    if (queryNew !== this.state.query) {
-      this.setState({ images: [], total: 0, page: 1 });
-      return this.setState({ query: queryNew });
+  const handleFormSubmit = queryNew => {
+    if (queryNew !== query) {
+      return;
+      setQuery(queryNew);
+      setImages([]);
+      setTotal(0);
+      setPage(1);
     }
-    return toast.info('You already have it 🥳');
+    return toast.info('You have already loaded it 🥳');
   };
 
-  createGallery = async (query, page) => {
+  const createGallery = async (query, page) => {
     try {
-      this.setState({ loading: true });
+      setLoading(true);
       const data = await fetch(query, page);
       if (!data.totalHits) {
         return toast.error(
           'Sorry, something went wrong, change your request 🧐'
         );
       } else {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...data.hits],
-          total: data.totalHits,
-        }));
+        setTotal(data.totalHits);
+        setImages(prevState => [...prevState, ...data.hits]);
       }
     } catch (error) {
       console.error(error);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  componentDidUpdate(_, prevState) {
-    const { page, query } = this.state;
+  useEffect(() => {
+    createGallery(page, query);
+  }, [page, query]);
 
-    if (prevState.page !== page || prevState.query !== query) {
-      this.createGallery(query, page);
-    }
-  }
-
-  render() {
-    return (
-      <Wrapper>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery images={this.state.images} />
-        {this.state.loading && <Loader />}
-        <Toast />
-        {this.state.total > this.state.images.length && (
-          <Button onLoadMore={this.loadMore} />
-        )}
-      </Wrapper>
-    );
-  }
+  return (
+    <Wrapper>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery images={images} />
+      {loading && <Loader />}
+      <Toast />
+      {total > images.length && <Button onLoadMore={loadMore} />}
+    </Wrapper>
+  );
 }
+
+// export class AppOld extends Component {
+//   state = {
+//     query: '',
+//     page: 1,
+//     images: [],
+//     total: 0,
+//     loading: false,
+
+//   };
+
+//   handleFormSubmit = queryNew => {
+//     if (queryNew !== this.state.query) {
+//       this.setState({ images: [], total: 0, page: 1 });
+//       return this.setState({ query: queryNew });
+//     }
+//     return toast.info('You already have it 🥳');
+//   };
+
+//   createGallery = async (query, page) => {
+//     try {
+//       this.setState({ loading: true });
+//       const data = await fetch(query, page);
+//       if (!data.totalHits) {
+//         return toast.error(
+//           'Sorry, something went wrong, change your request 🧐'
+//         );
+//       } else {
+//         this.setState(prevState => ({
+//           images: [...prevState.images, ...data.hits],
+//           total: data.totalHits,
+//         }));
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       this.setState({ loading: false });
+//     }
+//   };
+
+//   loadMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     const { page, query } = this.state;
+
+//     if (prevState.page !== page || prevState.query !== query) {
+//       this.createGallery(query, page);
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <Wrapper>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery images={this.state.images} />
+//         {this.state.loading && <Loader />}
+//         <Toast />
+//         {this.state.total > this.state.images.length && (
+//           <Button onLoadMore={this.loadMore} />
+//         )}
+//       </Wrapper>
+//     );
+//   }
+// }
